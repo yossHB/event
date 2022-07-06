0390db36974039be06f59c93b47d3260c62cd3af
yossHB <yyosr00@gmail.com>
2022-07-06 08:03:37 +0100

    backend

---
+++ event_spring_boot/src/test/java/en/univercity/event/EventApplicationTests.java
@@ -0,0 +1,13 @@
+package en.univercity.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
